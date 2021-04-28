75da0e7fd0d94743904e4355ac02df70647f96c4
Ruby Ou <rubyruru3205@gmail.com>
2021-04-28 16:24:06 +0800

    react_hello build

--- src/cgu_multiButton.js
+++ src/cgu_multiButton.js
@@ -1,3 +1,5 @@
+import Button from '@material-ui/core/Button';
+
 const changeText=(event)=>{
     console.log(event.target)
     event.target.innerText = event.target.innerText + "被點了了"
@@ -6,8 +8,8 @@ const changeText=(event)=>{
 const MultiButton=(num)=>{
     var output=[];
     for(let i=1;i<num+1;++i){
-        output.push(<button 
-        onClick={changeText}>第{i}個按鍵</button>)
+        output.push(<Button variant="outlined" color="primary"
+        onClick={changeText}>第{i}個按鍵</Button>)
     }
     return output;
 }
